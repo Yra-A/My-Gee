ac3ead6986e4f915996fe7977bbf897cfa7c7c69
Yra-A <1325083019@qq.com>
2023-07-09 08:27:07 +0800

    implement router group

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ This repo is my implementation of [7days-golang/gee-web at master · geektutu/7d
 Gee is a Gin-like web framework.
 
 *   Day1：utilizes the HTTP library to construct a preliminary prototype of the Gee framework.
-*   Day2：implements Context that encapsulates the request, responseWriter, and other related information.
-*   Day3: implements Trie to parse routing.
+*   Day2：implement Context that encapsulates the request, responseWriter, and other related information.
+*   Day3: implement Trie to parse routing.
+*   Day4：implement router group
 
---
+++ day4_group/gee/gee.go
@@ -0,0 +1,66 @@
+package gee
+
+import (
+	"log"
+	"net/http"
+)
+
+type HandlerFunc func(*Context)
+
+type routerGroup struct {
+	prefix      string
+	middlewares []HandlerFunc // 当前分组支持的中间件
+	parent      *routerGroup  // 父分组
+	engine      *Engine       // 所有分组共享一个 Engine
+}
+
+type Engine struct {
+	*routerGroup
+	router *router
+	groups []*routerGroup // 存储了所有的分组
+}
+
+func New() *Engine {
+	engine := &Engine{router: newRouter()}
+	engine.routerGroup = &routerGroup{engine: engine}
+	engine.groups = []*routerGroup{engine.routerGroup}
+	return engine
+}
+
+// 在当前分组下创建一个新的子分组
+func (group *routerGroup) Group(prefix string) *routerGroup {
+	newGroup := &routerGroup{
+		prefix: group.prefix + prefix,
+		parent: group,
+		engine: group.engine,
+	}
+	group.engine.groups = append(group.engine.groups, newGroup)
+	return newGroup
+}
+
+func (group *routerGroup) addRoute(method string, comp string, handler HandlerFunc) {
+	pattern := group.prefix + comp
+	log.Printf("Route %4s - %s", method, pattern)
+	group.engine.router.addRoute(method, pattern, handler)
+}
+
+// add GET request
+func (group *routerGroup) GET(pattern string, handler HandlerFunc) {
+	group.addRoute("GET", pattern, handler)
+}
+
+// add POST request
+func (group *routerGroup) POST(pattern string, handler HandlerFunc) {
+	group.addRoute("POST", pattern, handler)
+}
+
+// run and start a http server
+func (engine *Engine) Run(addr string) (err error) {
+	return http.ListenAndServe(addr, engine)
+}
+
+// 实现 http.Handler 接口的 ServeHTTP 方法
+func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	c := newContext(w, req) // 用响应和请求，创建一个Context
+	engine.router.handle(c) // 调用对应处理函数
+}
